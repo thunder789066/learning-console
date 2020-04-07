332c35723e845b047fd64f88dfe3603102cc0ebb
Christina Gerstner <christinalgerstner@gmail.com>
2020-04-07 18:30:39 -0500

    Initial commit

---
+++ programlearn1/programlearn1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programlearn1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            String FirstName;
+            Console.WriteLine("whats your first name?");
+            String LastName;
+            FirstName = Console.ReadLine();
+            Console.WriteLine("whats your last name?");
+            LastName = Console.ReadLine();
+            Console.WriteLine("welcome: "+LastName+","+" "+FirstName);
+
+
+
+                Console.Read();
+        }
+    }
+}
